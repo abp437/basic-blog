6e3fb56dfa27781ab6129eb1bb3d9b88bc3ef3a7
abp437 <abp437@gmail.com>
2021-11-01 11:56:38 +0530

    Add comment for the purpose of resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # The resources routes add up all the CRUD routes
   resources :posts do
     # These routes are nested within Posts
     resources :comments
